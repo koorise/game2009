eb14dbeaf6a245cf422d0ef8f8832c009af4505b
koorise@gmail.com <koorise@gmail.com@fcc77f68-0f73-11de-99c7-9d618077940f>
2009-04-17 03:39:24 +0000

    Bin目录下增加Asp.net AjaxControlToolkit.dll空间库。 Login.ascx 文件修改，可以使用了。 Reg.ascx 用户注册控件完成 css/global.css 增加一部分CSS [koorise]

---
+++ WebUserControl/Reg.ascx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using GameDB;
+using SubSonic;
+
+public partial class WebUserControl_Reg : System.Web.UI.UserControl
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void lBTNCode_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnConfirm_Click(object sender, EventArgs e)
+    {
+        if(Session["chkCode"].ToString()==tbCode.Text)
+        {
+            Query q = new Query("gUserInfo");
+            q.AddWhere("username", tbUserName.Text).OR("uMail",tbEmail1.Text);
+            if(q.GetRecordCount()==0)
+            {
+                GUserInfo.Insert(tbUserName.Text, tbPassWD1.Text, int.Parse(ddlQuestion.SelectedValue), tbAnswer1.Text,
+                             tbEmail1.Text, Request.UserHostAddress, DateTime.Now);
+            }
+            else
+            {
+                Literal1.Text = "<script language=javascript>alert('该用户名或E-mail地址已注册！');</script>";
+            }
+            
+
+        }
+        else
+        {
+            Literal1.Text = "<script language=javascript>alert('验证码错误！');</script>";
+        }
+    }
+    protected void imgBTNChkUserName_Click(object sender, ImageClickEventArgs e)
+    {
+        Query q = new Query("gUserInfo");
+        q.AddWhere("userName", tbUserName.Text);
+        if(q.GetRecordCount()==0)
+        {
+            tbUserName.BackColor = System.Drawing.Color.GreenYellow;
+        }
+        else
+        {
+            tbUserName.BackColor = System.Drawing.Color.Maroon;
+        }
+    }
+}
